5b4a78287e5ee3f357581d1649baca6b51f3398b
Junior <Junior@Yoga>
2019-10-19 13:49:38 +0200

    added ping function

--- gui_main.py
+++ gui_main.py
@@ -15,6 +15,9 @@ from wol import wake_on_lan
 from wol import loadConfig
 from wol import check_mac
 
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+
 
 def btn_action(id="def"):
     host = btn[id].cget("text")
@@ -144,6 +147,21 @@ btn5.grid(row = 2, column = 0)
 
 
 ################################## Functions
+
+
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0
 
 def quit_func():
     if (mp.is_working()):
